b3d50679ef63add3c5e45ddbf3b84058a2819359
toqaalaa20 <90696437+toqaalaa20@users.noreply.github.com>
2023-09-01 01:49:35 +0300

    Intial commit

---
+++ tracker.py
@@ -0,0 +1,68 @@
+from deep_sort import nn_matching
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from deep_sort import generate_detections as gdet
+import numpy as np
+
+
+class Object_Tracker:
+    tracker = None
+    encoder = None
+    tracks = None
+
+    def __init__(self):
+
+        max_cosine_distance = 0.7
+        nn_budget = None
+        model_filename = 'model_data/mars-small128.pb'
+        self.encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+        metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+        self.tracker = Tracker(metric)
+        
+
+
+    def update(self, frame, detections):
+
+        if len(detections) == 0:
+            self.tracker.predict()
+            self.tracker.update([])  
+            self.update_tracks()
+            return
+
+        bboxes = np.asarray([d[:-2] for d in detections])
+        bboxes[:, 2:] = bboxes[:, 2:] - bboxes[:, 0:2]
+        scores = [d[-2] for d in detections]
+        names = [d[-1] for d in detections]
+
+        features = self.encoder(frame, bboxes)
+
+        dets = []
+        for bbox_id, bbox in enumerate(bboxes):
+            dets.append(Detection(bbox, scores[bbox_id], names[bbox_id], features[bbox_id]))
+
+        self.tracker.predict()
+        self.tracker.update(dets)
+        self.update_tracks()
+
+    def update_tracks(self):
+        tracks = []
+        for track in self.tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            bbox = track.to_tlbr()
+
+            id = track.track_id
+
+            tracks.append(Track(id, bbox))
+
+        self.tracks = tracks
+
+
+class Track:
+    track_id = None
+    bbox = None
+
+    def __init__(self, id, bbox):
+        self.track_id = id
+        self.bbox = bbox
